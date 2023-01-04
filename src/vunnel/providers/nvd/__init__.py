d8715f4447126cd2c104ad80f990433e8cd201e6
Alex Goodman <wagoodman@users.noreply.github.com>
2023-01-04 12:18:22 -0500

    rewrite nvd provider to use NVD API v2 (#27)
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>
    
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- src/vunnel/providers/nvd/__init__.py
+++ src/vunnel/providers/nvd/__init__.py
@@ -1,44 +1,56 @@
+import os
 from dataclasses import dataclass, field
 
 from vunnel import provider, schema
+from vunnel.providers.nvd.manager import Manager
 
-from .parser import Parser
 
-
-@dataclass(frozen=True)
+@dataclass
 class Config:
     runtime: provider.RuntimeConfig = field(default_factory=provider.RuntimeConfig)
     request_timeout: int = 125
     start_year: int = 2002
     end_year: int | None = None
+    api_key: str = "env:NVD_API_KEY"
+
+    def __post_init__(self) -> None:
+        if self.api_key.startswith("env:"):
+            self.api_key = os.environ.get(self.api_key[4:], "")
+
+    def __str__(self) -> str:
+        # sanitize secrets from any output
+        api_value = self.api_key
+        str_value = super().__str__()
+        if not api_value:
+            return str_value
+        return str_value.replace(api_value, "********")
 
 
 class Provider(provider.Provider):
-    def __init__(self, root: str, config: Config):
+    def __init__(self, root: str, config: Config) -> None:
         super().__init__(root, runtime_cfg=config.runtime)
         self.config = config
+        self.schema = schema.NVDSchema()
+        self.manager = Manager(
+            workspace=self.workspace,
+            download_timeout=self.config.request_timeout,
+            start_year=self.config.start_year,
+            end_year=self.config.end_year,
+            api_key=self.config.api_key,
+            logger=self.logger,
+        )
 
     @classmethod
     def name(cls) -> str:
         return "nvd"
 
     def update(self) -> tuple[list[str], int]:
-        parser = Parser(
-            workspace=self.workspace.input_path,
-            download_timeout=self.config.request_timeout,
-            start_year=self.config.start_year,
-            end_year=self.config.end_year,
-            logger=self.logger,
-        )
-
-        vuln_tuples = parser.get(skip_if_exists=self.config.runtime.skip_if_exists)
-
         with self.results_writer() as writer:
-            for cve_id, cve in vuln_tuples:
+            for identifier, record in self.manager.get(skip_if_exists=self.config.runtime.skip_if_exists):
                 writer.write(
-                    identifier=cve_id.lower(),
-                    schema=schema.NVDSchema(),
-                    payload=cve,
+                    identifier=identifier.lower(),
+                    schema=self.schema,
+                    payload=record,
                 )
 
-        return parser.urls, len(writer)
+        return self.manager.urls, len(writer)
