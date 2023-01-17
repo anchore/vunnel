66b2b06b9a2eb9647acf55e8536c4d49e5d66066
Alex Goodman <wagoodman@users.noreply.github.com>
2023-01-17 14:27:23 -0500

    better human readible display of the config (#45)
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>
    
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- src/vunnel/cli/cli.py
+++ src/vunnel/cli/cli.py
@@ -1,5 +1,7 @@
 import dataclasses
+import enum
 import logging
+from typing import Any
 
 import click
 import yaml
@@ -77,7 +79,51 @@ def show_config(cfg: config.Application) -> None:
         def increase_indent(self, flow: bool = False, indentless: bool = False) -> None:
             return super().increase_indent(flow, False)
 
-    cfg_dict = dataclasses.asdict(cfg)
+    def enum_asdict_factory(data: list[tuple[str, Any]]) -> dict[Any, Any]:
+        # prevents showing oddities such as
+        #
+        #   wolfi:
+        #       request_timeout: 125
+        #       runtime:
+        #       existing_input: !!python/object/apply:vunnel.provider.InputStatePolicy
+        #           - keep
+        #       existing_results: !!python/object/apply:vunnel.provider.ResultStatePolicy
+        #           - delete-before-write
+        #       on_error:
+        #           action: !!python/object/apply:vunnel.provider.OnErrorAction
+        #           - fail
+        #           input: !!python/object/apply:vunnel.provider.InputStatePolicy
+        #           - keep
+        #           results: !!python/object/apply:vunnel.provider.ResultStatePolicy
+        #           - keep
+        #           retry_count: 3
+        #           retry_delay: 5
+        #       result_store: !!python/object/apply:vunnel.result.StoreStrategy
+        #           - flat-file
+        #
+        # and instead preferring:
+        #
+        #   wolfi:
+        #       request_timeout: 125
+        #       runtime:
+        #       existing_input: keep
+        #       existing_results: delete-before-write
+        #       on_error:
+        #           action: fail
+        #           input: keep
+        #           results: keep
+        #           retry_count: 3
+        #           retry_delay: 5
+        #       result_store: flat-file
+
+        def convert_value(obj: Any) -> Any:
+            if isinstance(obj, enum.Enum):
+                return obj.value
+            return obj
+
+        return {k: convert_value(v) for k, v in data}
+
+    cfg_dict = dataclasses.asdict(cfg, dict_factory=enum_asdict_factory)
     print(yaml.dump(cfg_dict, Dumper=IndentDumper, default_flow_style=False))
 
 
