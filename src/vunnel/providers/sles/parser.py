f7163a4065d8473f045d184c6e50680ee0aebfe3
Daniel Nurmi <nurmi@anchore.com>
2023-06-23 09:53:52 -0700

    chore: make dataclass representations for AdvisorySummary and VendorAdvisory so that Module and VendorAdvisory (both optional) could be added to the FixedIn dataclass specification
    Signed-off-by: Daniel Nurmi <nurmi@anchore.com>

--- src/vunnel/providers/sles/parser.py
+++ src/vunnel/providers/sles/parser.py
@@ -295,6 +295,8 @@ class Parser:
                             NamespaceName=feed_ns,
                             VersionFormat="rpm",
                             Version=pkg_version,
+                            Module=None,
+                            VendorAdvisory=None,
                         )
                     )
 
