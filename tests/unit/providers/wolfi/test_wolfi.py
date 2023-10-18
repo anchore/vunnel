70c318c4edc2170570225ed1a3a38693c4440a36
Alex Goodman <wagoodman@users.noreply.github.com>
2023-10-18 15:26:03 -0400

    add wolfi snapshot tests (#345)
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- tests/unit/providers/wolfi/test_wolfi.py
+++ tests/unit/providers/wolfi/test_wolfi.py
@@ -188,16 +188,34 @@ def disable_get_requests(monkeypatch):
 
 
 def test_provider_schema(helpers, disable_get_requests):
-    workspace = helpers.provider_workspace_helper(name=Provider.name())
-
+    workspace = helpers.provider_workspace_helper(
+        name=Provider.name(),
+        input_fixture="test-fixtures/input",
+    )
     c = Config()
     c.runtime.result_store = result.StoreStrategy.FLAT_FILE
     p = Provider(root=workspace.root, config=c)
-
-    mock_data_path = helpers.local_dir("test-fixtures/input")
-    shutil.copytree(mock_data_path, workspace.input_dir, dirs_exist_ok=True)
 
     p.update(None)
 
     assert workspace.num_result_entries() == 50
     assert workspace.result_schemas_valid(require_entries=True)
+
+
+def test_provider_via_snapshot(helpers, disable_get_requests, monkeypatch):
+    workspace = helpers.provider_workspace_helper(
+        name=Provider.name(),
+        input_fixture="test-fixtures/input",
+    )
+
+    c = Config()
+    # keep all of the default values for the result store, but override the strategy
+    c.runtime.result_store = result.StoreStrategy.FLAT_FILE
+    p = Provider(
+        root=workspace.root,
+        config=c,
+    )
+
+    p.update(None)
+
+    workspace.assert_result_snapshots()
