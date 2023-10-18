526a8c7304c9317e8b0334e8ddf7a925a7b7093e
William Murphy <will.murphy@anchore.com>
2023-10-18 14:31:36 -0400

    chore: test chainguard provider via snapshots (#339)
    Signed-off-by: Will Murphy <will.murphy@anchore.com>

--- tests/unit/providers/chainguard/test_chainguard.py
+++ tests/unit/providers/chainguard/test_chainguard.py
@@ -32,3 +32,27 @@ def test_provider_schema(helpers, disable_get_requests):
 
     assert workspace.num_result_entries() == 189
     assert workspace.result_schemas_valid(require_entries=True)
+
+
+def test_provider_via_snapshot(helpers, disable_get_requests, monkeypatch):
+    workspace = helpers.provider_workspace_helper(
+        name=Provider.name(),
+        input_fixture="test-fixtures/input",
+    )
+
+    c = Config()
+    # keep all of the default values for the result store, but override the strategy
+    c.runtime.result_store = result.StoreStrategy.FLAT_FILE
+    p = Provider(
+        root=workspace.root,
+        config=c,
+    )
+
+    def mock_download():
+        return None
+
+    monkeypatch.setattr(p.parser, "_download", mock_download)
+
+    p.update(None)
+
+    workspace.assert_result_snapshots()
