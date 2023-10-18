89de0a1957eeab0551b63ecd4baddb99c6c606a6
Alex Goodman <wagoodman@users.noreply.github.com>
2023-10-18 14:53:02 -0400

    Add snapshot tests for NVD provider (#340)
    * enable multiple snapshot tests
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    * add snapshot tests for nvd provider
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- tests/unit/providers/nvd/test_nvd.py
+++ tests/unit/providers/nvd/test_nvd.py
@@ -1,10 +1,20 @@
 from __future__ import annotations
 
+import os
 import json
 
 import pytest
 from vunnel import provider, result
 from vunnel.providers import nvd
+from vunnel.providers.nvd import api as nvd_api
+
+
+@pytest.fixture()
+def disable_get_requests(monkeypatch):
+    def disabled(*args, **kwargs):
+        raise RuntimeError("requests disabled but HTTP GET attempted")
+
+    monkeypatch.setattr(nvd_api.requests, "get", disabled)
 
 
 @pytest.mark.parametrize(
@@ -32,7 +42,7 @@ def test_incremental_update_with_existing_results(policy, should_raise):
         ("test-fixtures/single-entry.json", 1),
     ],
 )
-def test_provider_schema(helpers, mock_data_path, expected_written_entries, mocker):
+def test_provider_schema(helpers, mock_data_path, expected_written_entries, disable_get_requests, mocker):
     workspace = helpers.provider_workspace_helper(name=nvd.Provider.name())
     mock_data_path = helpers.local_dir(mock_data_path)
 
@@ -48,3 +58,38 @@ def test_provider_schema(helpers, mock_data_path, expected_written_entries, mock
 
     assert expected_written_entries == workspace.num_result_entries()
     assert workspace.result_schemas_valid(require_entries=expected_written_entries > 0)
+
+
+@pytest.mark.parametrize(
+    ("mock_data_path",),
+    [
+        ("test-fixtures/single-entry.json",),
+        ("test-fixtures/full-page.json",),
+    ],
+)
+def test_provider_via_snapshot(helpers, mock_data_path, disable_get_requests, mocker):
+    snapshot_name = os.path.basename(mock_data_path).split(".")[0]
+
+    workspace = helpers.provider_workspace_helper(
+        name=nvd.Provider.name(),
+        snapshot_prefix=snapshot_name,
+    )
+
+    c = nvd.Config()
+    # keep all of the default values for the result store, but override the strategy
+    c.runtime.result_store = result.StoreStrategy.FLAT_FILE
+    p = nvd.Provider(
+        root=workspace.root,
+        config=c,
+    )
+
+    mock_data_path = helpers.local_dir(mock_data_path)
+
+    with open(mock_data_path) as f:
+        json_dict = json.load(f)
+
+    p.manager.api.cve = mocker.Mock(return_value=[json_dict])
+
+    p.update(None)
+
+    workspace.assert_result_snapshots()
