8574ec916ce3044d0eb3fb9e9743a896444e28c5
Will Murphy <will.murphy@anchore.com>
2023-10-23 15:54:29 -0400

    suppress qa issues in NVD manager
    Signed-off-by: Will Murphy <will.murphy@anchore.com>

--- src/vunnel/providers/nvd/manager.py
+++ src/vunnel/providers/nvd/manager.py
@@ -3,12 +3,14 @@ from __future__ import annotations
 import datetime
 import logging
 import os
-from collections.abc import Generator
-from typing import Any
-
-from vunnel.workspace import Workspace
+from typing import TYPE_CHECKING, Any
 
 from .api import NvdAPI
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
+
+    from vunnel.workspace import Workspace
 
 
 class Manager:
@@ -26,13 +28,15 @@ class Manager:
         self.logger = logger
 
         self.api = NvdAPI(api_key=api_key, logger=logger, timeout=download_timeout)
-        self.urls = [self.api._cve_api_url_]
+        self.urls = [self.api._cve_api_url_]  # noqa: SLF001
 
     def get(
-        self, last_updated: datetime.datetime | None, skip_if_exists: bool = False,
+        self,
+        last_updated: datetime.datetime | None,
+        skip_if_exists: bool = False,
     ) -> Generator[tuple[str, dict[str, Any]], Any, None]:
         if skip_if_exists and self._can_update_incrementally(last_updated):
-            yield from self._download_updates(last_updated)  # type: ignore
+            yield from self._download_updates(last_updated)  # type: ignore  # noqa: PGH003
         else:
             yield from self._download_all()
 
@@ -45,7 +49,7 @@ class Manager:
 
         if days_since_last_sync >= NvdAPI.max_date_range_days:
             self.logger.info(
-                f"last sync was {days_since_last_sync} days ago (more than {NvdAPI.max_date_range_days} days, the max range value of the NVD API), downloading all data",
+                f"last sync was {days_since_last_sync} days ago (more than {NvdAPI.max_date_range_days} days, the max range value of the NVD API), downloading all data",  # noqa: E501
             )
             return False
 
