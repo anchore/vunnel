8920f5767956c5487bedaafed67eef90bbf07be2
William Murphy <will.murphy@anchore.com>
2023-11-17 10:25:42 -0500

    refactor: new http wrapper in rhel provider (#392)
    Use new http wrapper in RHEL provider. Add ability to pass custom
    status handler to http wrapper so that RHEL provider can request
    not to retry 404s.
    
    Signed-off-by: Will Murphy <will.murphy@anchore.com>

--- tests/unit/providers/rhel/test_rhel.py
+++ tests/unit/providers/rhel/test_rhel.py
@@ -511,14 +511,6 @@ class TestParser:
         assert Parser._get_name_version(package) == (name, version)
 
 
-@pytest.fixture
-def disable_get_requests(monkeypatch):
-    def disabled(*args, **kwargs):
-        raise RuntimeError("requests disabled but HTTP GET attempted")
-
-    monkeypatch.setattr(parser.requests, "get", disabled)
-
-
 def test_provider_schema(helpers, disable_get_requests, monkeypatch):
     workspace = helpers.provider_workspace_helper(
         name=Provider.name(),
