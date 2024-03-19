4d54514f89a6576af2d833d7115b510226910b1a
Will Murphy <will.murphy@anchore.com>
2024-03-19 14:52:07 -0400

    update and tests for safe_extract_tar
    Now that we're using it for more than one thing, make an extractor that
    generally prevents path traversal.
    
    Signed-off-by: Will Murphy <will.murphy@anchore.com>

--- src/vunnel/utils/archive.py
+++ src/vunnel/utils/archive.py
@@ -1,17 +1,22 @@
 import os
 import tarfile
+from pathlib import Path
+
 
 def filter_path_traversal(tarinfo: tarfile.TarInfo, path: str) -> tarfile.TarInfo | None:
-    # we do not expect any relative file paths that would result in the clean
-    # path being different from the original path
+    # drop any path that would result in a write outside the destination dir
     # e.g.
-    #  expected:   results/results.db
-    #  unexpected: results/../../../../etc/passwd
-    # we filter (drop) any such entries
+    #   allowed: './some-dir/file.txt'
+    #   not allowed: 'some-dir/../../../../../etc/passwd'
+    dest_dir = Path(os.path.abspath(path))
+    write_path = Path(os.path.normpath(os.path.join(dest_dir, tarinfo.name)))
 
-    if tarinfo.name != os.path.normpath(tarinfo.name):
-        return None
-    return tarinfo
+    if dest_dir in write_path.parents:
+        return tarinfo
+    return None
+
 
 def safe_extract_tar(tar: tarfile.TarFile, destination_dir: str) -> None:
-    tar.extractall(destination_dir, filter=filter_path_traversal)
+    # explanation of noqa: S202
+    # This function is a safe wrapper around tar.extractall.
+    tar.extractall(destination_dir, filter=filter_path_traversal) # noqa: S202
--- tests/unit/utils/test_archive.py
+++ tests/unit/utils/test_archive.py
@@ -3,18 +3,21 @@ from vunnel.utils.archive import filter_path_traversal
 
 import pytest
 
+
 @pytest.mark.parametrize(
     "tar_info_name, allowed",
     [
-        # ("file.txt", True),
+        ("file.txt", True),
         ("./file.txt", True),
-        # ("../file.txt", False),
-        # ("/file.txt", False),
-    ]
+        ("some-dir/file.txt", True),
+        ("../file.txt", False),
+        ("/file.txt", False),
+        ("some-dir/../../../../../../etc/passwd", False),
+    ],
 )
 def test_filter_path_traversal(tar_info_name: str, allowed: bool):
     tar_info = tarfile.TarInfo(tar_info_name)
-    actual = filter_path_traversal(tar_info, "")
+    actual = filter_path_traversal(tar_info, "/some/path")
     if allowed:
         assert actual is not None
         assert actual.name == tar_info_name
