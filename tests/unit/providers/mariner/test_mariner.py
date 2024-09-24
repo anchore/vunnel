470557e7ec4b8e1089c24542b93d0722212e4197
William Murphy <willmurphyscode@users.noreply.github.com>
2024-09-24 14:58:21 -0400

    feat: add azurelinux3 vuln feed (#569)
    * feat: add azurelinux3 URLs
    
    Includes re-generating models.
    
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>
    
    * add unit tests for azure linux 3 support
    
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>
    
    * test: add vulnerable range to azurelinux3 test
    
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>
    
    * test: update quality gate to use Azure Linux 3 image and labels
    
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>
    
    * chore: appease linter
    
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>
    
    * chore: fix azure linux 3 reference
    
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>
    
    * chore: bump vuln match labels
    
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>
    
    * chore: use grype main
    
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>

--- tests/unit/providers/mariner/test_mariner.py
+++ tests/unit/providers/mariner/test_mariner.py
@@ -8,7 +8,7 @@ from pytest_unordered import unordered
 from vunnel import result, workspace, utils
 from vunnel.providers.mariner import Config, Provider, parser
 from vunnel.providers.mariner.parser import MarinerXmlFile
-from vunnel.utils.vulnerability import Vulnerability, FixedIn
+from vunnel.utils.vulnerability import Vulnerability, FixedIn, VendorAdvisory
 
 
 @pytest.mark.parametrize(
@@ -97,7 +97,72 @@ from vunnel.utils.vulnerability import Vulnerability, FixedIn
                     ],
                 ),
             ],
-        )
+        ),
+        (
+            "test-fixtures/azure-linux-truncated-3.0-oval.xml",
+            [
+                Vulnerability(
+                    Name="CVE-2024-24258",
+                    NamespaceName="mariner:3.0",
+                    Description="CVE-2024-24258 affecting package freeglut for versions less than 3.4.0-1. A patched version of the package is available.",
+                    Severity="High",
+                    Link="https://nvd.nist.gov/vuln/detail/CVE-2024-24258",
+                    CVSS=[],
+                    FixedIn=[
+                        FixedIn(
+                            Name="freeglut",
+                            NamespaceName="mariner:3.0",
+                            VersionFormat="rpm",
+                            Version="0:3.4.0-1.azl3",
+                            VulnerableRange="< 0:3.4.0-1.azl3",
+                            Module="",
+                            VendorAdvisory=VendorAdvisory(NoAdvisory=False, AdvisorySummary=[]),
+                        )
+                    ],
+                    Metadata={},
+                ),
+                Vulnerability(
+                    Name="CVE-2024-24259",
+                    NamespaceName="mariner:3.0",
+                    Description="CVE-2024-24259 affecting package freeglut for versions less than 3.4.0-1. A patched version of the package is available.",
+                    Severity="High",
+                    Link="https://nvd.nist.gov/vuln/detail/CVE-2024-24259",
+                    CVSS=[],
+                    FixedIn=[
+                        FixedIn(
+                            Name="freeglut",
+                            NamespaceName="mariner:3.0",
+                            VersionFormat="rpm",
+                            Version="0:3.4.0-1.azl3",
+                            VulnerableRange="< 0:3.4.0-1.azl3",
+                            Module="",
+                            VendorAdvisory=VendorAdvisory(NoAdvisory=False, AdvisorySummary=[]),
+                        )
+                    ],
+                    Metadata={},
+                ),
+                Vulnerability(
+                    Name="CVE-2020-27304",
+                    NamespaceName="mariner:3.0",
+                    Description="CVE-2020-27304 affecting package ceph for versions less than 18.2.1-1. An upgraded version of the package is available that resolves this issue.",
+                    Severity="Critical",
+                    Link="https://nvd.nist.gov/vuln/detail/CVE-2020-27304",
+                    CVSS=[],
+                    FixedIn=[
+                        FixedIn(
+                            Name="ceph",
+                            NamespaceName="mariner:3.0",
+                            VersionFormat="rpm",
+                            Version="0:18.2.1-1.azl3",
+                            VulnerableRange="< 0:18.2.1-1.azl3",
+                            Module="",
+                            VendorAdvisory=VendorAdvisory(NoAdvisory=False, AdvisorySummary=[]),
+                        )
+                    ],
+                    Metadata={},
+                ),
+            ],
+        ),
     ],
 )
 def test_parse(tmpdir, helpers, input_file, expected):
