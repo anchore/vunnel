eed08af15ad9daf7eb860646f6fdef899d81a94a
Will Murphy <willmurphyscode@users.noreply.github.com>
2024-11-07 11:22:48 -0500

    test: first unit test for new CSAF parser
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>

---
+++ tests/unit/providers/rhel_csaf/test_rhel_csaf_document.py
@@ -0,0 +1,33 @@
+import pytest
+from pathlib import Path
+
+import orjson
+
+from vunnel.providers.rhel_csaf.csaf_document import RHEL_CSAFDocument
+
+FIXTURE_DIR = Path(__file__).parent / "test-fixtures"
+
+# Parameterized data structure for specific field assertions
+csaf_test_cases = [
+        "cve-2021-46661.json",
+]
+
+
+def get_csaf_document_path(filename):
+    return FIXTURE_DIR / filename
+
+def get_result_json_for_path(csaf_filename):
+    result_file = FIXTURE_DIR / f"result-for-{csaf_filename}"
+    with open(result_file) as FH:
+        return orjson.loads(FH.read())
+
+
+@pytest.mark.parametrize("csaf_filename", csaf_test_cases)
+def test_vuln_basic_data(csaf_filename):
+    csaf_path = get_csaf_document_path(csaf_filename)
+    results = get_result_json_for_path(csaf_filename)
+    doc = RHEL_CSAFDocument.from_path(csaf_path)
+    for vuln in results:
+        assert vuln.get("item", {}).get("Vulnerability", {}).get("Name") == doc.cve_id
+        assert vuln.get("item", {}).get("Vulnerability", {}).get("Severity") == doc.severity
+        assert vuln.get("item", {}).get("Vulnerability", {}).get("Link") == doc.vuln_url
