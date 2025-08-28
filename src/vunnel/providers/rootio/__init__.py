5dcec5d5148ed43f8c28b96c0145c075aa337b5a
Shaked Dembo <shaked.dembo@root.io>
2025-08-28 12:22:27 +0300

    Add rootio provider for Rooi.io CVE feed * Added following the existing directory structure a provider folder for rootio plus a provider class and a parser * Added to the global cli config the rootio provider config mapping * Added the rootio provider to the mapping in the global provider initialization * Added the appropriate configuration for testing the new provider with a unit test
    Signed-off-by: Shaked Dembo <shaked.dembo@root.io>

---
+++ src/vunnel/providers/rootio/__init__.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import os
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING
+
+from vunnel import provider, result, schema
+from .parser import Parser
+
+if TYPE_CHECKING:
+    import datetime
+
+
+@dataclass
+class Config:
+    runtime: provider.RuntimeConfig = field(
+        default_factory=lambda: provider.RuntimeConfig(
+            result_store=result.StoreStrategy.SQLITE,
+            existing_results=result.ResultStatePolicy.DELETE_BEFORE_WRITE,
+        ),
+    )
+    request_timeout: int = 125
+
+
+class Provider(provider.Provider):
+    __schema__ = schema.OSSchema()
+    __distribution_version__ = int(__schema__.major_version)
+
+    _url = "https://api.root.io/external/cve_feed"
+
+    def __init__(self, root: str, config: Config | None = None):
+        if not config:
+            config = Config()
+        super().__init__(root, runtime_cfg=config.runtime)
+        self.config = config
+
+        self.parser = Parser(
+            workspace=self.workspace,
+            url=self._url,
+            download_timeout=self.config.request_timeout,
+            logger=self.logger,
+        )
+
+    @classmethod
+    def name(cls) -> str:
+        return "rootio"
+
+    def update(self, last_updated: datetime.datetime | None) -> tuple[list[str], int]:
+        with self.results_writer() as writer:
+            for namespace, vuln_id, record in self.parser.get():
+                writer.write(
+                    identifier=os.path.join(namespace, vuln_id.lower()),
+                    schema=self.__schema__,
+                    payload=record,
+                )
+
+        return [self._url], len(writer)
