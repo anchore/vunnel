7ab8ade3bb3655769d8bc1c5aec22adf8c9f7c47
Alex Goodman <wagoodman@users.noreply.github.com>
2025-08-29 13:52:16 -0400

    Add fix date processing for ubuntu (#856)
    * add fix date processing for ubuntu
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    * rm dead code
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- tests/unit/cli/test_cli.py
+++ tests/unit/cli/test_cli.py
@@ -502,6 +502,7 @@ providers:
       skip_download: false
       skip_newer_archive_check: false
   ubuntu:
+    add_fix_dates: false
     additional_versions: {}
     enable_rev_history: true
     git_branch: master
