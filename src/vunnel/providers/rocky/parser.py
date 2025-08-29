df7df370a521f1e4397af4e77923b3ee5fcafb01
Alex Goodman <wagoodman@users.noreply.github.com>
2025-08-29 15:31:18 -0400

    Add fix date processing for rocky and bitnami (#858)
    * add fix-date to rocky
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    * add bitnami fix date processing
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    * test with dates not datetime
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- src/vunnel/providers/rocky/parser.py
+++ src/vunnel/providers/rocky/parser.py
@@ -6,22 +6,33 @@ from typing import TYPE_CHECKING, Any
 
 import orjson
 
+from vunnel.utils import osv
+
 if TYPE_CHECKING:
     from collections.abc import Generator
 
+    from vunnel.tool import fixdate
     from vunnel.workspace import Workspace
 
 from .client import Client
 
 
 class Parser:
-    def __init__(self, ws: Workspace, logger: logging.Logger | None = None, skip_download: bool = False):
+    def __init__(
+        self,
+        ws: Workspace,
+        fixdater: fixdate.Finder | None = None,
+        logger: logging.Logger | None = None,
+        skip_download: bool = False,
+    ):
+        self.fixdater = fixdater
         self.workspace = ws
         if not logger:
             logger = logging.getLogger(self.__class__.__name__)
         self.logger = logger
         self.client = Client(
             download_path=os.path.join(self.workspace.input_path, "osv"),
+            fixdater=fixdater,
             logger=self.logger,
             skip_download=skip_download,
         )
@@ -39,5 +50,14 @@ class Parser:
         return vuln_id, vuln_schema, vuln_entry
 
     def get(self) -> Generator[tuple[str, str, dict[str, Any]], None, None]:
-        for vuln_entry in self._load():
-            yield self._normalize(vuln_entry)
+        for advisory in self._load():
+            osv.patch_fix_date(advisory, self.fixdater, ecosystem_processor=normalize_ecosystem)
+            yield self._normalize(advisory)
+
+
+def normalize_ecosystem(ecosystem: str) -> str:
+    # e.g. take "Rocky Linux:8" and other versions and convert to "rocky:8"
+    parts = ecosystem.split(":")
+    if len(parts) == 2 and parts[0].lower() == "rocky linux":
+        return f"rocky:{parts[1]}"
+    return ecosystem.lower()
