04d7938f5b01ec51162001b56ac3013f920901a3
Will Murphy <willmurphyscode@users.noreply.github.com>
2026-05-04 16:49:20 -0400

    add unit tests for maven cgr libs
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>

--- tests/unit/providers/chainguard_libraries/test_openvex_parser.py
+++ tests/unit/providers/chainguard_libraries/test_openvex_parser.py
@@ -116,11 +116,13 @@ class TestOpenVEXParser:
     def test_load_skips_index_file(self, openvex_parser, helpers):
         openvex_parser.output_path = helpers.local_dir("test-fixtures/input/openvex")
         files = [f for f in openvex_parser._load()]
-        assert len(files) == 2  # pypi/joblib.openvex.json and urllib3.openvex.json
+        # pypi/joblib.openvex.json, pypi/urllib3.openvex.json, maven/spring-security-web.openvex.json
+        assert len(files) == 3
 
-        # Verify both files are loaded and index file (all.json) is skipped
+        # Verify all are loaded under their ecosystem dir name and index file (all.json) is skipped
         file_names = [f[0] for f in files]
         assert "pypi" in file_names
+        assert "maven" in file_names
 
     @pytest.mark.parametrize(
         "test_data,expected",
